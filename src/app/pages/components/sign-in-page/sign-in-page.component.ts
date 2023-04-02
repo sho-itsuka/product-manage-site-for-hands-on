6c05173f9e01f06097aeaeb5cd86b583b4ce2bf3
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-02 22:39:15 +0900

    言語化の設定

--- src/app/pages/components/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-in-page',
   templateUrl: './sign-in-page.component.html',
   styleUrls: ['./sign-in-page.component.scss']
 })
-export class SignInPageComponent {
+export class SignInPageComponent implements OnInit{
+
+  constructor(public translateService: TranslateService) { }
+
+  /**
+  * on init
+  */
+  ngOnInit(): void {
+  // Sets language from browser settings.
+  this.setupLanguage();
+}
+
+  // --------------------------------------------------------------------------------
+// private methods
+// --------------------------------------------------------------------------------
+private setupLanguage() {
+  // Setups language using browser settings.
+  this.translateService.setDefaultLang(this.getLanguage(navigator.language));
+  this.translateService.use(this.getLanguage(navigator.language));
+}
+
+private getLanguage(language: string): string {
+  console.log('SignInPageComponent #getLanguage() language:' + language);
+
+  const CHAR_HYPHEN = '-';
+  if (language.indexOf(CHAR_HYPHEN) > 0) {
+    const splittedLanguage: string[] = language.split(CHAR_HYPHEN);
+    console.log('SignInPageComponent #getLanguage() splittedLanguage[0]:' + splittedLanguage[0]);
+
+    return splittedLanguage[0];
+  }
+  return language;
+}
 
 }
