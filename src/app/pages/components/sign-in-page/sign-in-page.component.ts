a1489e4f5b3922d4a8a74a6b4d558728a7b12228
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-05 23:45:55 +0900

    サインインボタン押下時のメソッド追加

--- src/app/pages/components/sign-in-page/sign-in-page.component.ts
+++ src/app/pages/components/sign-in-page/sign-in-page.component.ts
@@ -1,9 +1,13 @@
+import { Observable } from 'rxjs';
 import { RoutingService } from 'src/app/core/services/routing.service';
 
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { TranslateService } from '@ngx-translate/core';
 
+import { UrlConst } from '../../constants/url-const';
+import { SignInRequestDto } from '../../models/dtos/requests/sign-in-request-dto';
+import { SignInResponseDto } from '../../models/dtos/responses/sign-in-response-dto';
 import { AccountService } from '../../services/account.service';
 
 @Component({
@@ -21,8 +25,8 @@ export class SignInPageComponent implements OnInit {
   });
 
   constructor(
-    private accountService: AccountService,
-    private routingService: RoutingService,
+    private accountService:   AccountService,
+    private routingService:   RoutingService,
     private formBuilder:      FormBuilder,
     public  translateService: TranslateService
     ) { }
@@ -33,6 +37,17 @@ export class SignInPageComponent implements OnInit {
   ngOnInit(): void {
   // Sets language from browser settings.
   this.setupLanguage();
+  }
+
+  /**
+  * Clicks sign in button
+  */
+  clickSignInButton() {
+    // Creates request dto.
+    const signInRequestDto = this.createSignInRequestDto();
+
+    // Signs in using dto.
+    this.signIn(signInRequestDto);
   }
 
   // --------------------------------------------------------------------------------
@@ -55,6 +70,25 @@ export class SignInPageComponent implements OnInit {
       return splittedLanguage[0];
     }
     return language;
+  }
+
+  private signIn(signInRequestDto: SignInRequestDto) {
+    // Signs in and gets response dto.
+    const signInResponseDto: Observable<SignInResponseDto> = this.accountService.signIn(signInRequestDto);
+    signInResponseDto.subscribe(responseDto => {
+      if (responseDto != null) {
+        // Moves to the Product listing page.
+        this.routingService.navigate(UrlConst.PATH_PRODUCT_LISTING);
+      }
+    });
+  }
+
+  private createSignInRequestDto(): SignInRequestDto {
+    // Creates Request.
+    return {
+      Username: this.signInUserAccount.value,
+      Password: this.signInUserPassword.value
+    };
   }
 
 }
