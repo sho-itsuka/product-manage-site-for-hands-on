0d888145b17c1bfa0a5a428139a91a1d5cfeb46c
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-05 23:08:18 +0900

    Account サービスを編集

--- src/app/pages/services/account.service.ts
+++ src/app/pages/services/account.service.ts
@@ -1,9 +1,38 @@
+import { catchError, Observable, of } from 'rxjs';
+
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+import { ApiConst } from '../constants/api-const';
+import { SignInRequestDto } from '../models/dtos/requests/sign-in-request-dto';
+import { SignInResponseDto } from '../models/dtos/responses/sign-in-response-dto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  /**
+  * Signs in
+  * @param signInRequestDto sign in request
+  * @returns sign in response
+  */
+signIn(signInRequestDto: SignInRequestDto): Observable<SignInResponseDto> {
+  const webApiUrl = ApiConst.PATH_API_ROOT + ApiConst.PATH_SIGN_IN;
+  const headers   = new HttpHeaders({
+    authorization: 'Basic ' + btoa(signInRequestDto.Username + ':' + signInRequestDto.Password)
+  });
+
+  return this.http
+    .post<SignInResponseDto>(webApiUrl, signInRequestDto, { headers })
+    .pipe(
+      catchError((error) => {
+        return of(null as SignInResponseDto);
+      })
+    );
+}
 }
