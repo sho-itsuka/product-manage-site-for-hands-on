e8d1f49177da957fe8cc9dec1c022061b6f633ae
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-14 23:40:03 +0900

    コンストラクタ追加

--- src/app/shared/services/title-i18.service.ts
+++ src/app/shared/services/title-i18.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TitleI18Service {
-
-  constructor() { }
+  constructor(
+    private translateService: TranslateService,
+    public title:             Title
+  ) {}
 }
