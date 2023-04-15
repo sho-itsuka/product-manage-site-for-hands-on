2f1b2f20151f0b5e81ed038d5b25efd8f08e2396
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-15 17:40:02 +0900

    メソッド追加

--- src/app/shared/services/title-i18.service.ts
+++ src/app/shared/services/title-i18.service.ts
@@ -8,6 +8,16 @@ import { TranslateService } from '@ngx-translate/core';
 export class TitleI18Service {
   constructor(
     private translateService: TranslateService,
-    public title:             Title
+    public  title:             Title
   ) {}
+
+  /**
+  * Sets title
+  * @param screenName Name of screen
+  */
+public setTitle(screenName: string) {
+  const titleSystem = this.translateService.instant('title.system');
+  const titleSub    = this.translateService.instant('title.' + screenName);
+  this.title.setTitle(titleSystem + titleSub);
+}
 }
