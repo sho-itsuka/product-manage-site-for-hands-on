7f45caf05d75585c7d9617e1aa9991afad661a78
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-15 18:40:49 +0900

    xhr-interceptor の組み込み

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import { ErrorMessagingComponent } from './components/error-messaging/error-messaging.component';
 import { LoadingComponent } from './components/loading/loading.component';
+import { XhrInterceptor } from './interceptors/xhr.interceptor';
 
 @NgModule({
   declarations: [
@@ -15,6 +17,12 @@ import { LoadingComponent } from './components/loading/loading.component';
     CommonModule,
     MaterialModule,
     NgxTranslateModule
+  ],
+  providers: [
+    { provide:  HTTP_INTERCEPTORS,
+      useClass: XhrInterceptor,
+      multi:    true
+    }
   ],
   exports: [
     LoadingComponent,
