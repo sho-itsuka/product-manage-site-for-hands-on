1185413ff6cdc1e3cfb6bedc59744356e70dfff1
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-17 21:53:48 +0900

    yseNoDialogの生成

---
+++ src/app/core/components/yes-no-dialog/yes-no-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector:    'app-yes-no-dialog',
+  templateUrl: './yes-no-dialog.component.html',
+  styleUrls:  ['./yes-no-dialog.component.scss']
+})
+export class YesNoDialogComponent {
+
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -6,12 +6,14 @@ import { MaterialModule } from '../material/material.module';
 import { NgxTranslateModule } from '../ngx-translate/ngx-translate.module';
 import { ErrorMessagingComponent } from './components/error-messaging/error-messaging.component';
 import { LoadingComponent } from './components/loading/loading.component';
+import { YesNoDialogComponent } from './components/yes-no-dialog/yes-no-dialog.component';
 import { XhrInterceptor } from './interceptors/xhr.interceptor';
 
 @NgModule({
   declarations: [
     LoadingComponent,
-    ErrorMessagingComponent
+    ErrorMessagingComponent,
+    YesNoDialogComponent
   ],
   imports: [
     CommonModule,
