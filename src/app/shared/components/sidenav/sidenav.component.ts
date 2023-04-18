384380f6fe6fba74869356160a704734282bbec3
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-18 22:01:31 +0900

    コンストラクタ追加

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -1,5 +1,7 @@
+import { RoutingService } from 'src/app/core/services/routing.service';
 import { UrlConst } from 'src/app/pages/constants/url-const';
 import { MenuListResponseDto } from 'src/app/pages/models/dtos/responses/menu-list-response-dto';
+import { AccountService } from 'src/app/pages/services/account.service';
 
 import { Component, EventEmitter, Output } from '@angular/core';
 
@@ -18,5 +20,10 @@ export class SidenavComponent {
 
   // Menu response data
   menuListResponseDto: MenuListResponseDto[];
+
+  constructor(
+    private accountService: AccountService,
+    public routingService:  RoutingService
+  ) {}
 
 }
