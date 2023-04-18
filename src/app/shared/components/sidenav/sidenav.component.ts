677b11d9654328b305056f575d559d5a79ff9894
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-18 22:03:26 +0900

    メソッド追加

--- src/app/shared/components/sidenav/sidenav.component.ts
+++ src/app/shared/components/sidenav/sidenav.component.ts
@@ -3,14 +3,14 @@ import { UrlConst } from 'src/app/pages/constants/url-const';
 import { MenuListResponseDto } from 'src/app/pages/models/dtos/responses/menu-list-response-dto';
 import { AccountService } from 'src/app/pages/services/account.service';
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector:    'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls:  ['./sidenav.component.scss']
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
 
   // Clicks sidenav and throw event
   @Output() sidenavClose = new EventEmitter();
@@ -25,5 +25,36 @@ export class SidenavComponent {
     private accountService: AccountService,
     public routingService:  RoutingService
   ) {}
+
+  /**
+   * on init
+   */
+  ngOnInit(): void {
+    this.getMenu();
+    // }
+  }
+
+  /**
+   * Clicks submenu
+   */
+  clickSubmenu(): void {
+    this.sidenavClose.emit();
+  }
+
+  /**
+   * Clicks home
+   */
+  public clickHome(): void {
+    this.sidenavClose.emit();
+  }
+
+  // --------------------------------------------------------------------------------
+  // private methods
+  // --------------------------------------------------------------------------------
+  private getMenu(): void {
+    this.accountService.getMenu().subscribe((menuListResponseDto) => {
+      this.menuListResponseDto = menuListResponseDto;
+    });
+  }
 
 }
