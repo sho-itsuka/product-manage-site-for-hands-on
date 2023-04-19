808d7658be4ee2e2d3651e0137c349628eb2aee8
sho-itsuka <sho.itsuka122@gmail.com>
2023-04-20 00:03:04 +0900

    コンストラクター追加

--- src/app/pages/guards/auth.guard.ts
+++ src/app/pages/guards/auth.guard.ts
@@ -1,15 +1,25 @@
+import { Observable } from 'rxjs';
+import { RoutingService } from 'src/app/core/services/routing.service';
+
+import { state } from '@angular/animations';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+
+import { AccountService } from '../services/account.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(
+    private accountService: AccountService,
+    private routingService: RoutingService
+  ) {}
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return true;
   }
-  
+
 }
