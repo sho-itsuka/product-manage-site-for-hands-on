0bc9e79702645db7f643bf68678cf2c7dde7f521
sho-itsuka <sho.itsuka122@gmail.com>
2023-05-04 18:35:49 +0900

    インターフェイス作成

--- src/app/pages/models/dtos/requests/product-listing-search-params-dto.ts
+++ src/app/pages/models/dtos/requests/product-listing-search-params-dto.ts
@@ -1,2 +1,8 @@
-export interface ProductListingSearchParamsDto {
+import { BaseSearchParamsDto } from './base-search-params-dto';
+
+export interface ProductListingSearchParamsDto extends BaseSearchParamsDto {
+  productName:  string;
+  productCode:  string;
+  productGenre: string;
+  endOfSale:    boolean;
 }
